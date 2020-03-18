7ce9a59bbd0bf543b15bf11d18f0727393e8d028
Abhay <abhay@clinicsoncloud.com>
2020-03-18 17:04:58 +0530

    connection to new server using localhost

--- app/src/main/java/com/abhaybmicoc/app/utils/ApiUtils.java
+++ app/src/main/java/com/abhaybmicoc/app/utils/ApiUtils.java
@@ -2,7 +2,8 @@ package com.abhaybmicoc.app.utils;
 
 public class ApiUtils {
     private static String PROTOCOL = "http://";
-    private static String SERVER_URL = "45.252.190.29";
+//    private static String SERVER_URL = "45.252.190.29";
+    private static String SERVER_URL = "192.168.0.115:7777";
 
     public static String KIOSK = PROTOCOL + SERVER_URL + "/api/v1/kiosk";
     public static String LOGIN_URL = PROTOCOL + SERVER_URL + "/api/v1/login";
